f355825aded73c0345473d3f5ea3a23d4d0c1e74
Bruno Alla <browniebroke@users.noreply.github.com>
2018-01-21 17:08:56 +0000

    Link pip to pip3 in brew_mac.sh

--- brew_mac.sh
+++ brew_mac.sh
@@ -12,6 +12,9 @@ brew bundle
 # Make Python 3 system default.
 rm -fr /usr/local/bin/python
 ln -s /usr/local/bin/python3 /usr/local/bin/python
+# As well as pip
+rm -rf /usr/local/bin/pip
+ln -s /usr/local/bin/pip3 /usr/local/bin/pip
 
 # Manually
 echo "Please login to Dropbox and wait for mackup data to be synchronised."
