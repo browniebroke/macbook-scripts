f22ae02bd54815850d7d955fd57c40fc99cbc349
Bruno Alla <browniebroke@users.noreply.github.com>
2019-08-16 11:59:55 +0200

    Ignore if command line tools are already installed

--- brew_mac.sh
+++ brew_mac.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Install Xcode Command Line Tools.
-xcode-select --install
+xcode-select --install || :
 
 # Install Homebrew.
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
